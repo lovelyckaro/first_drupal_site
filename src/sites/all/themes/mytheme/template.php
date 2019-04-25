ea6b6ac5924af8b3d7db1bd1bb480f0047215e2b
Love Lyckaro <love.lyckaro@hotmail.se>
2019-04-25 13:46:44 +0200

    add theme hook suggestion for nodes in teaser view mode

--- src/sites/all/themes/mytheme/template.php
+++ src/sites/all/themes/mytheme/template.php
@@ -1,15 +1,20 @@
 <?php
-function mytheme_preprocess_image_style(&$vars) {
-    $vars['attributes']['class'][] = 'img-fluid';
-}
-
 function mytheme_preprocess_field(&$variables) {
     $field_name = $variables['element']['#field_name'];
-    if($field_name == 'field_event_image' || $field_name == "field_info_image"){
+    if($field_name == 'field_event_image' || $field_name == "field_info_image" || $field_name == "field_news_image"){
         foreach($variables['items'] as $key => $item){
             $variables['items'][ $key ]['#item']['attributes']['class'][] = 'img-fluid'; // http://getbootstrap.com/css/#overview-responsive-images
         }
     }
 }
+
+function mytheme_preprocess_node(&$vars) {
+    if($vars['view_mode'] == 'teaser') {
+      $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__teaser';   
+      $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->nid . '__teaser';
+    }
+  }
+
+
 
 ?>
